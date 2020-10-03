94c66aeececa6ca1954e1f320b063be9b75e1a9a
iamrajendra <rajendrarajaram@hotmail.com>
2020-10-03 15:35:30 +0530

    Implement the Koin

--- app/src/main/kotlin/com/raywenderlich/markme/feature/presenter/FeaturePresenter.kt
+++ app/src/main/kotlin/com/raywenderlich/markme/feature/presenter/FeaturePresenter.kt
@@ -26,11 +26,14 @@ import com.raywenderlich.markme.feature.FeatureContract
 import com.raywenderlich.markme.model.Student
 import com.raywenderlich.markme.repository.AppRepository
 import com.raywenderlich.markme.utils.ClassSection
+import org.koin.standalone.KoinComponent
+import org.koin.standalone.inject
 
-class FeaturePresenter(private var view: FeatureContract.View<Student>?) : FeatureContract.Presenter<Student> {
+class FeaturePresenter(private var view: FeatureContract.View<Student>?) : FeatureContract.Presenter<Student>
+, KoinComponent
+{
 
-    private val repository: FeatureContract.Model<Student> by lazy { AppRepository }
-
+   private val repository: FeatureContract.Model<Student> by inject()
     override fun onSave2PrefsClick(data: List<Student>?) {
         data?.let {
             repository.add2Prefs(data = data, callback = { msg ->
@@ -47,10 +50,15 @@ class FeaturePresenter(private var view: FeatureContract.View<Student>?) : Featu
         }
     }
 
-    override fun loadPersistedData(data: List<Student>, featureType: ClassSection) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  override fun loadPersistedData(data: List<Student>, featureType: ClassSection) {
+    when (featureType) {
+      ClassSection.ATTENDANCE -> repository.fetchFromPrefs(data)
+      ClassSection.GRADING -> repository.fetchFromDb(data = data,
+        callback = { loadedData ->
+          view?.onPersistedDataLoaded(loadedData)
+        })
     }
-
+  }
     override fun onViewDestroyed() {
         view = null
     }
--- app/src/main/kotlin/com/raywenderlich/markme/main/view/ui/MainActivity.kt
+++ app/src/main/kotlin/com/raywenderlich/markme/main/view/ui/MainActivity.kt
@@ -27,48 +27,50 @@ import android.support.v7.app.AppCompatActivity
 import com.raywenderlich.markme.R
 import com.raywenderlich.markme.feature.view.ui.FeatureActivity
 import com.raywenderlich.markme.main.MainContract
-import com.raywenderlich.markme.main.presenter.MainPresenter
 import com.raywenderlich.markme.utils.ClassSection
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
 import timber.log.Timber
 
 const val FEATURE_CATEGORY = "categoryName"
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
-    private val cardAtt by lazy { activity_main__cardview__attendance }
-    private val cardGrading by lazy { activity_main__cardview__grading }
-    private val mainPresenter: MainContract.Presenter by lazy { MainPresenter(this) }
+  private val cardAtt by lazy { activity_main__cardview__attendance }
+  private val cardGrading by lazy { activity_main__cardview__grading }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  //    private val mainPresenter: MainContract.Presenter by lazy { MainPresenter(this) }
+  private val mainPresenter: MainContract.Presenter by inject { parametersOf(this) }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        cardAtt.setOnClickListener {
-            mainPresenter.onAttendanceOptionClick()
-        }
-        cardGrading.setOnClickListener {
-            mainPresenter.onGradingOptionClick()
-        }
+    cardAtt.setOnClickListener {
+      mainPresenter.onAttendanceOptionClick()
     }
-
-    override fun navigateTo(section: ClassSection) {
-        when (section) {
-            ClassSection.ATTENDANCE -> {
-                Timber.d("'Attendance' clicked")
-                startActivity<FeatureActivity>(FEATURE_CATEGORY to ClassSection.ATTENDANCE)
-            }
-            ClassSection.GRADING -> {
-                Timber.d("'Grading' clicked")
-                startActivity<FeatureActivity>(FEATURE_CATEGORY to ClassSection.GRADING)
-            }
-        }
+    cardGrading.setOnClickListener {
+      mainPresenter.onGradingOptionClick()
     }
+  }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        mainPresenter.onViewDestroyed()
+  override fun navigateTo(section: ClassSection) {
+    when (section) {
+      ClassSection.ATTENDANCE -> {
+        Timber.d("'Attendance' clicked")
+        startActivity<FeatureActivity>(FEATURE_CATEGORY to ClassSection.ATTENDANCE)
+      }
+      ClassSection.GRADING -> {
+        Timber.d("'Grading' clicked")
+        startActivity<FeatureActivity>(FEATURE_CATEGORY to ClassSection.GRADING)
+      }
     }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    mainPresenter.onViewDestroyed()
+  }
 }
